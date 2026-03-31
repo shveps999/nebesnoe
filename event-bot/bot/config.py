8f65579887d66f122e6d3ce29fa2186cb2af0795
shveps999 <privetzdarovahappy@gmail.com>
2026-03-31 14:24:06 +0300

    Update config.py

--- event-bot/bot/config.py
+++ event-bot/bot/config.py
@@ -1,1 +1,8 @@
-dfdfdf
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+DB_PATH = os.getenv("DB_PATH", "bot/database.db")
